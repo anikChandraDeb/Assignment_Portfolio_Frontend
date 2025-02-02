74e6787a2a0c48f540a7486c71f91ca28a596adb
Anik Deb <anik.deb161@gmail.com>
2025-02-02 20:27:01 +0600

    navbar collapse

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,29 +3,44 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Navbar = () => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const handleCollapse = () => {
+        setIsCollapsed(!isCollapsed);  // Toggle collapse state
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">ANIK'S Sol<sup>n</sup></Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" 
+                    aria-expanded={!isCollapsed ? 'true' : 'false'} 
+                    aria-label="Toggle navigation"
+                    onClick={handleCollapse}  // Toggle on click
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
+                            <Link className="nav-link" to="/" onClick={handleCollapse}>Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
+                            <Link className="nav-link" to="/about" onClick={handleCollapse}>About</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/blog">Blog</Link>
+                            <Link className="nav-link" to="/blog" onClick={handleCollapse}>Blog</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/service">Services</Link>
+                            <Link className="nav-link" to="/service" onClick={handleCollapse}>Services</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/contact">Contact</Link>
+                            <Link className="nav-link" to="/contact" onClick={handleCollapse}>Contact</Link>
                         </li>
                     </ul>
                 </div>
