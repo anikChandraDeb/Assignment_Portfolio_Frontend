a689ad6760615928bc9a9bf9a23694bba6c07de8
Anik Deb <anik.deb161@gmail.com>
2025-02-03 21:01:16 +0600

    Update deshboard navbar solve collapse problem

--- src/components/DashboardNavbar.jsx
+++ src/components/DashboardNavbar.jsx
@@ -5,30 +5,45 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const DashboardNavbar = () => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    
+    const handleCollapse = () => {
+        setIsCollapsed(!isCollapsed);  // Toggle collapse state
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/dashboard">Dashboard</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#dashboardNavbar" aria-controls="dashboardNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#dashboardNavbar" 
+                    aria-controls="dashboardNavbar" 
+                    aria-expanded={!isCollapsed ? 'true' : 'false'} 
+                    aria-label="Toggle navigation"
+                    onClick={handleCollapse}  // Toggle on click
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="dashboardNavbar">
+                <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="dashboardNavbar">
                     <ul className="navbar-nav ms-auto">
                         
                         <li className="nav-item">
-                            <Link className="nav-link" to="/dashboard/blog">Manage Blogs</Link>
+                            <Link className="nav-link" to="/dashboard/blog"  onClick={handleCollapse}>Manage Blogs</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/dashboard/team">Manage Team</Link>
+                            <Link className="nav-link" to="/dashboard/team"  onClick={handleCollapse}>Manage Team</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/dashboard/service">Manage Services</Link>
+                            <Link className="nav-link" to="/dashboard/service"  onClick={handleCollapse}>Manage Services</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/dashboard/profile">Profile</Link>
+                            <Link className="nav-link" to="/dashboard/profile"  onClick={handleCollapse}>Profile</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/dashboard/logout"><FontAwesomeIcon icon={faSignOutAlt} /></Link>
+                            <Link className="nav-link" to="/dashboard/logout"  onClick={handleCollapse}><FontAwesomeIcon icon={faSignOutAlt} /></Link>
                         </li>
                     </ul>
                 </div>
