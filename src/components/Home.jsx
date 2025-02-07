33537f73d27d301808abae9295e1baa90c78c63f
Anik Deb <anik.deb161@gmail.com>
2025-02-07 22:16:21 +0600

    Hero section update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Blog from "./Blog"; 
 import Footer from "./Footer";
+import '../assets/css/Home.css';
+import anik from '../assets/images/anik.jpeg'
+
 const Home = () => {
   return (
     <>
       {/* Hero Section */}
-      <section className="text-center text-white bg-primary py-5">
-        <div className="container" Style="padding:20px">
-          <h1>Welcome to ANIKS Sol<sup>n</sup></h1>
-          <p>Showcasing my work and experiences</p>
-        </div>
-      </section>
+      <div className="container">
+            <div className="row custom-design">
+                <div className="col-6">
+                    <h2>Building Scalable & Efficient Web Application</h2>
+                    <p>I specialize in developing high-performance web applications with a focus on clean code, scalability, and seamless user experiences... </p>
+                   <button className="btn btn-primary"><Link className="nav-link" to="/contact">Contact Me</Link></button>
+                </div>
+                <div className="col-6">
+                    <img src={anik} alt="anik-image" />
+                </div>
+            </div>
+      </div>
 
       {/* Blog Section */}
       <Blog limit="6" />
@@ -20,3 +30,4 @@ const Home = () => {
 };
 
 export default Home;
+
