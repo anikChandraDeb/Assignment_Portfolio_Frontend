7a7285d80cc4c8c3618246e14408f6b45bb97da9
Anik Deb <anik.deb161@gmail.com>
2025-02-15 08:27:01 +0600

    react-scroll problem solved

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5050/api', 
+    baseURL: 'portfolio-backend-zeta-ashy.vercel.app/api', 
     headers: {
         'Content-Type': 'application/json',
     }
